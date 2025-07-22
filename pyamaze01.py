b8278ad709cd0dbbe79039d4de67664081b49af3
JUHO <ijuho9555@gmail.com>
2025-07-22 15:03:24 +0900

    m.tracePath구문 추가
    agent의 흔적 볼 수 있는 구문

--- pyamaze01.py
+++ pyamaze01.py
@@ -1,13 +1,18 @@
 # turtle02
 from pyamaze import maze,COLOR,agent
 
-m=maze(10,10)
+m=maze(20,20)
 m.CreateMaze(theme=COLOR.light)
-a=agent(m)
+a=agent(m,filled=True,footprints=True)
+m.tracePath({a:m.path})
 m.run()
 
 # loopPercent: 복잡성 나타냄
 
 # pattern='v': 벽이 이어지게끔 생성
 
-# COLOR와 agent는 import구문에 추가 
+# COLOR와 agent는 import구문에 추가
+
+# a.position=(x,y)로 agent를 여러개 생성 가능
+
+# m.tracePath({a:m.path})로 agent의 흔적을 볼 수 있음
