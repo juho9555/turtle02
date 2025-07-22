fcfc9b18dfbc43149fdf4d04b78c674419367238
JUHO <ijuho9555@gmail.com>
2025-07-22 14:55:12 +0900

    배경 색과 agent모드 추가

--- pyamaze01.py
+++ pyamaze01.py
@@ -1,7 +1,13 @@
 # turtle02
-from pyamaze import maze
+from pyamaze import maze,COLOR,agent
 
-m=maze(5,5)
-m.CreateMaze(5,5,pattern='v')
+m=maze(10,10)
+m.CreateMaze(theme=COLOR.light)
+a=agent(m)
 m.run()
 
+# loopPercent: 복잡성 나타냄
+
+# pattern='v': 벽이 이어지게끔 생성
+
+# COLOR와 agent는 import구문에 추가 
